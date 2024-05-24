1a60cbe9a6bd459b4336d3276cf808a6a8c3638f
domiluk <151018843+domiluk@users.noreply.github.com>
2024-05-24 21:45:37 +0200

    Inspect helper functions

--- helpers.php
+++ helpers.php
@@ -44,3 +44,30 @@ function loadPartial($name)
     echo "Partial '{$name}' not found.";
   }
 }
+
+/**
+ * Inspect a value
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+  die();
+}
