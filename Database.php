07d6c2fca496e2b160bc46154b2f90af22165329
domiluk <151018843+domiluk@users.noreply.github.com>
2024-05-26 20:19:34 +0200

    Single listing display

--- Database.php
+++ Database.php
@@ -29,13 +29,20 @@ class Database
    * Query the database
    * 
    * @param string $query
+   * @param array $params
    * @return PDOStatement
    * @throws PDOException
    */
-  public function query($query)
+  public function query($query, $params = [])
   {
     try {
       $stmt = $this->conn->prepare($query);
+
+      // Bind named params
+      foreach ($params as $param => $value) {
+        $stmt->bindValue(':' . $param, $value);
+      }
+
       $stmt->execute();
       return $stmt;
     } catch (PDOException $e) {
