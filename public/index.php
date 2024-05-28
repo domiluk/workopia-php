0de07cbe4018eaa0f9eb6097c24bfcea916dae58
domiluk <151018843+domiluk@users.noreply.github.com>
2024-05-28 10:40:37 +0200

    Session class & set user

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
-
-session_start();
-
 require __DIR__ . '/../vendor/autoload.php';
-require '../helpers.php';
 
 use Framework\Router;
+use Framework\Session;
 
+Session::start();
+
+require '../helpers.php';
 // spl_autoload_register(function ($class) {
 //   $path = basePath('Framework/' . $class . '.php');
 //   if (file_exists($path)) {
